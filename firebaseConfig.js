4849fb5c01c308165f17a72b28d6ae652ad71e02
carlosmantilla23 <carlos.mantilla.jaimes@gmail.com>
2024-09-13 22:35:51 -0500

    test

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Importar las funciones necesarias de Firebase SDK
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+// Tu configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCdPupjN6iIZHOVK1NcqI-hrt_VEgA2T7g",
   authDomain: "pollamania-4a7ef.firebaseapp.com",
@@ -13,7 +13,13 @@ const firebaseConfig = {
   appId: "1:1738147394:web:a0fb8286ad6c9baf2f0fdb"
 };
 
-// Initialize Firebase
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-export { app };
+// Inicializar Firebase Authentication
+const auth = getAuth(app);
+
+// Inicializar Firestore
+const firestore = getFirestore(app);
+
+export { app, auth, firestore };
